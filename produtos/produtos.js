731bb276b69c27a49536c7c89ea1130a8e33d424
achegas <150072616+achegas@users.noreply.github.com>
2023-11-07 16:47:25 -0300

    Update produtos.js

--- produtos/produtos.js
+++ produtos/produtos.js
@@ -1,4 +1,6 @@
 // produtos.js
+// Substitua a importação da função format do money-formatter por esta linha
+const formatMoney = (value) => new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(value);
 
 // Função para obter produtos do localStorage ou usar produtos padrão
 function getStoredProducts() {
@@ -6,58 +8,73 @@ function getStoredProducts() {
     return storedProducts ? JSON.parse(storedProducts) : getDefaultProducts();
 }
 
+// Função auxiliar para verificar se o preço está dentro da faixa selecionada
+function isPriceInRange(productPrice, selectedValue) {
+    const [max] = selectedValue.split("-").map(value => parseFloat(value.replace(",", ".")));
+
+    return !isNaN(productPrice) && productPrice <= max;
+}
+
+
 // Função para obter produtos padrão
 function getDefaultProducts() {
     return [
+        {  
+            name: "Mouse Gamer",
+            category: ["Eletrônicos", "Todos"],
+            image: "https://m.media-amazon.com/images/I/71gbhPToXdL._AC_SX425_.jpg",
+            price: "R$ 22,90",
+            amazonLink: "https://amzn.to/3QJj4Dj",
+            date: new Date("2023-10-06"), // Adicione uma data para representar a data de lançamento
+        },
         {
-        name: "Multilaser MO251 - Mouse Sem Fio 2.4 Ghz 1200 DPI Usb, Preto, normal",
-        category: ["Eletrônicos", "Todos"],
-        image: "https://m.media-amazon.com/images/I/71gbhPToXdL._AC_SX425_.jpg",
-        price: "R$ 22,90",
-        amazonLink: "https://amzn.to/3QJj4Dj"
-    },
-    {
-        name: "Meia Masculina Kit 12 Pares Cano Alto Sport Algodão Tamanho 39 a 43",
-        category: ["Roupas", "Todos"],
-        image: "https://m.media-amazon.com/images/I/61dnjb5-VhL._AC_SX679_.jpg",
-        price: "59,90",
-        amazonLink: "https://amzn.to/461shLy"
-    },
-    {
-        name: "Kit 5 Camisetas Masculinas Básicas Lisa Poliéster Premium",
-        category: ["Roupas", "Todos"],
-        image: "https://m.media-amazon.com/images/I/41CAIk6E-EL._AC_SX679_.jpg",
-        price: "99,90",
-        amazonLink: "https://amzn.to/3QKviLS"
-    },
-    {
-        name: "Greenote Aspirador sem fios, aspirador de vara 23000PA 6 em 1, potente aspirador leve para casa de 200W com faróis LED, tempo de funcionamento de 35 minutos, chão duro, carro para animais de estimação",
-        category: ["Casa", "Todos"],
-        image: "https://m.media-amazon.com/images/I/61yqC+pw5gL._AC_SX425_.jpg",
-        price: "1.594,78",
-        amazonLink: "https://amzn.to/3Mth6V0"
-    },
-    {
-        name: "Fritadeira Sem Óleo Air Fryer 4L, Mondial, Preto/Inox, 1500W, 110V ou 220 - AFN-40-BI",
-        category: ["Cozinha", "Todos"],
-        image: "https://m.media-amazon.com/images/I/71zeBuMlz2L._AC_SY879_.jpg",
-        price: "369,00",
-        amazonLink: "https://amzn.to/47mRUYt"
-    },
-    {
-        name: "Processador AMD Ryzen 7 5800X, Cache 36MB, 3.8GHz (4.7GHz Max Turbo), AM4",
-        category: ["Eletrônicos", "Todos"],
-        image: "https://m.media-amazon.com/images/I/61IIbwz-+ML._AC_SX425_.jpg",
-        price: "1.519,00",
-        amazonLink: "https://amzn.to/466tpxw"
-    },
-    {
-        name: "Kit Frio Conjunto Termico Pelinho Elasticidade Segunda Pele Unissex",
-        category: ["Roupas", "Todos"],
-        image: "https://m.media-amazon.com/images/I/512JffOdCBL._AC_SX679_.jpg",
-        price: "R$77,99 - R$89,80",
-        amazonLink: "https://amzn.to/40r1uHq"
-    },
+            name: "Meia Masculina Kit 12 Pares Cano Alto Sport Algodão Tamanho 39 a 43",
+            category: ["Roupas", "Todos"],
+            image: "https://m.media-amazon.com/images/I/61dnjb5-VhL._AC_SX679_.jpg",
+            price: "R$ 59,90",
+            amazonLink: "https://amzn.to/461shLy",
+            date: new Date("2023-10-06"), // Adicione uma data para representar a data de lançamento
+        },
+        {
+            name: "Kit 5 Camisetas Masculinas Básicas Lisa Poliéster Premium",
+            category: ["Roupas", "Todos"],
+            image: "https://m.media-amazon.com/images/I/41CAIk6E-EL._AC_SX679_.jpg",
+            price: "R$ 99,90",
+            amazonLink: "https://amzn.to/3QKviLS",
+            date: new Date("2023-10-05"), // Adicione uma data para representar a data de lançamento
+        },
+        {
+            name: "Greenote Aspirador sem fios, aspirador de vara 23000PA 6 em 1, potente aspirador leve para casa de 200W com faróis LED, tempo de funcionamento de 35 minutos, chão duro, carro para animais de estimação",
+            category: ["Casa", "Todos"],
+            image: "https://m.media-amazon.com/images/I/61yqC+pw5gL._AC_SX425_.jpg",
+            price: "R$ 1.594,78",
+            amazonLink: "https://amzn.to/3Mth6V0",
+            date: new Date("2023-10-05"), // Adicione uma data para representar a data de lançamento
+        },
+        {
+            name: "Fritadeira Sem Óleo Air Fryer 4L, Mondial, Preto/Inox, 1500W, 110V ou 220 - AFN-40-BI",
+            category: ["Cozinha", "Todos"],
+            image: "https://m.media-amazon.com/images/I/71zeBuMlz2L._AC_SY879_.jpg",
+            price: "R$ 369,00",
+            amazonLink: "https://amzn.to/47mRUYt",
+            date: new Date("2023-10-05"), // Adicione uma data para representar a data de lançamento
+        },
+        {
+            name: "Processador AMD Ryzen 7 5800X, Cache 36MB, 3.8GHz (4.7GHz Max Turbo), AM4",
+            category: ["Eletrônicos", "Todos"],
+            image: "https://m.media-amazon.com/images/I/61IIbwz-+ML._AC_SX425_.jpg",
+            price: "R$ 1.519,00",
+            amazonLink: "https://amzn.to/466tpxw",
+            date: new Date("2023-10-05"), // Adicione uma data para representar a data de lançamento
+        },
+        {
+            name: "Kit Frio Conjunto Termico Pelinho Elasticidade Segunda Pele Unissex",
+            category: ["Roupas", "Todos"],
+            image: "https://m.media-amazon.com/images/I/512JffOdCBL._AC_SX679_.jpg",
+            price: "R$ 89,80",
+            amazonLink: "https://amzn.to/40r1uHq",
+            date: new Date("2023-10-05"), // Adicione uma data para representar a data de lançamento
+        },
         // ... (outros produtos)
     ];
 }
@@ -75,7 +92,10 @@ function addProductToList(product, currentRow, category) {
 
     image.src = product.image;
     name.innerText = product.name;
-    price.innerText = product.price;
+    
+    	const numericPrice = parseFloat(product.price.replace("R$", "").replace(".", "").replace(",", "."));
+    	
+    price.innerText = formatMoney(product.price);
     buyButton.innerText = "Comprar";
     buyButton.href = product.amazonLink;
     buyButton.target = "_blank";  // Abrir em uma nova aba
@@ -94,94 +114,188 @@ function addProductToList(product, currentRow, category) {
     currentRow.appendChild(listItem);
 }
 
-// Função para realizar a pesquisa
-function search() {
-    const searchTerm = document.getElementById("search").value.toLowerCase();
-    const dynamicContent = document.getElementById("product-list");
-    dynamicContent.innerHTML = '';
-
-    let currentRow;
-    let foundResults = false;
-
-    for (const product of products) {
-        // Verifica se o termo de pesquisa está presente no nome do produto
-        if (product.name.toLowerCase().includes(searchTerm)) {
-            if (!currentRow) {
-                currentRow = document.createElement("div");
-                currentRow.className = "product-row";
-                dynamicContent.appendChild(currentRow);
+        document.getElementById("search").addEventListener("keypress", function (event) {
+            if (event.key === "Enter") {
+                search();
             }
-            addProductToList(product, currentRow, 'Pesquisa');
-            foundResults = true;
+        });
+
+        const menuButtons = document.querySelectorAll('nav a');
+
+        function setActiveButton(button) {
+            menuButtons.forEach(btn => btn.classList.remove('active'));
+            button.classList.add('active');
         }
-    }
 
-    // Adiciona mensagem se nenhum resultado for encontrado
-    if (!foundResults) {
-        const noResultsMessage = document.createElement("p");
-        noResultsMessage.innerText = "Nenhum resultado encontrado.";
-        dynamicContent.appendChild(noResultsMessage);
-    }
-}
+        menuButtons.forEach(button => {
+            button.addEventListener('click', function () {
+                setActiveButton(button);
+                loadDynamicContent(button.innerText, button.innerText.toLowerCase());
+            });
+        });
 
-// Função para filtrar por preço
-function filterByPrice() {
-    const priceFilter = document.getElementById("price-filter").value;
-    const dynamicContent = document.getElementById("product-list");
-    dynamicContent.innerHTML = '';
+        function loadDynamicContent(category, categoryClass) {
+            const dynamicContent = document.getElementById("product-list");
+            dynamicContent.innerHTML = '';
 
-    let currentRow;
+            document.getElementById("search").value = '';
 
-    for (const product of products) {
-        const productPrice = parseFloat(product.price.replace("R$", "").replace(",", "."));
+            let currentRow;
 
-        if ((priceFilter === "" || (priceFilter === "0-50" && productPrice <= 50) ||
-            (priceFilter === "51-100" && productPrice > 50 && productPrice <= 100) ||
-            (priceFilter === "101-200" && productPrice > 100 && productPrice <= 200))) {
-            if (!currentRow) {
-                currentRow = document.createElement("div");
-                currentRow.className = "product-row";
-                dynamicContent.appendChild(currentRow);
+            for (const product of products) {
+                if (category === 'Inicio' || product.category.includes(category)) {
+                    if (!currentRow) {
+                        currentRow = document.createElement("div");
+                        currentRow.className = "product-row " + categoryClass;  
+                        dynamicContent.appendChild(currentRow);
+                    }
+                    addProductToList(product, currentRow, categoryClass);  
+                }
             }
-            addProductToList(product, currentRow, 'Filtro');
         }
-    }
-}
 
-// Função para ordenar os produtos
-function sortBy() {
-    const sortOrder = document.getElementById("sort-order").value;
-    const dynamicContent = document.getElementById("product-list");
-    dynamicContent.innerHTML = '';
+        const priceRangeInput = document.getElementById("price-range");
+        priceRangeInput.addEventListener("input", updatePriceRange);
 
-    let sortedProducts = [...products];
+        function updatePriceRange() {
+            const selectedPriceElement = document.getElementById("selected-price");
+            const selectedValue = priceRangeInput.value.replace(",", ".");
+            const numericValue = parseFloat(selectedValue);
 
-    if (sortOrder === "recentes") {
-        sortedProducts.sort((a, b) => new Date(b.date) - new Date(a.date));
-    } else if (sortOrder === "preco-asc") {
-        sortedProducts.sort((a, b) => parseFloat(a.price.replace("R$", "").replace(",", ".")) - parseFloat(b.price.replace("R$", "").replace(",", ".")));
-    } else if (sortOrder === "preco-desc") {
-        sortedProducts.sort((a, b) => parseFloat(b.price.replace("R$", "").replace(",", ".")) - parseFloat(a.price.replace("R$", "").replace(",", ".")));
-    }
-
-    let currentRow;
-
-    for (const product of sortedProducts) {
-        if (!currentRow) {
-            currentRow = document.createElement("div");
-            currentRow.className = "product-row";
-            dynamicContent.appendChild(currentRow);
+            if (!isNaN(numericValue)) {
+                selectedPriceElement.innerText = `R$ 0 - R$ ${numericValue.toFixed(2)}`;
+                filterByPriceRange(numericValue);
+            } else {
+                console.error("Invalid price range value");
+            }
         }
-        addProductToList(product, currentRow, 'Ordenado');
-    }
-}
 
+        function addProductToList(product, currentRow, category) {
+            const listItem = document.createElement("li");
+            const image = document.createElement("img");
+            const name = document.createElement("p");
+            const price = document.createElement("p");
+            const buyButton = document.createElement("a");
 
-// Chamada inicial para carregar o conteúdo padrão (Todos os produtos)
-window.addEventListener('load', function () {
-    loadDynamicContent('Todos');
-});
+            image.src = product.image;
+            name.innerText = product.name;
+            price.innerText = product.price;
+            buyButton.innerText = "Comprar";
+            buyButton.href = product.amazonLink;
+            buyButton.target = "_blank";
 
-// Chamada inicial para carregar o conteúdo padrão (Todos os produtos)
-// loadDynamicContent('Todos');
+            price.classList.add("price");
 
+            listItem.setAttribute("data-category", category);
+
+            listItem.appendChild(image);
+            listItem.appendChild(name);
+            listItem.appendChild(price);
+            listItem.appendChild(buyButton);
+
+            currentRow.appendChild(listItem);
+        }
+
+        function search() {
+            const searchTerm = document.getElementById("search").value.toLowerCase();
+            const dynamicContent = document.getElementById("product-list");
+            dynamicContent.innerHTML = '';
+
+            let currentRow;
+            let foundResults = false;
+
+            for (const product of products) {
+                if (product.name.toLowerCase().includes(searchTerm)) {
+                    if (!currentRow) {
+                        currentRow = document.createElement("div");
+                        currentRow.className = "product-row";
+                        dynamicContent.appendChild(currentRow);
+                    }
+                    addProductToList(product, currentRow, 'Pesquisa');
+                    foundResults = true;
+                }
+            }
+
+            if (!foundResults) {
+                const noResultsMessage = document.createElement("p");
+                noResultsMessage.innerText = "Nenhum resultado encontrado.";
+                dynamicContent.appendChild(noResultsMessage);
+            }
+        }
+
+        function filterByPriceRange(rangeValue) {
+            const dynamicContent = document.getElementById("product-list");
+            dynamicContent.innerHTML = '';
+
+            let currentRow;
+            let foundResults = false;
+
+            for (const product of products) {
+                const productPrice = parseFloat(product.price.replace("R$", "").replace(".", "").replace(",", "."));
+
+                if (!isNaN(productPrice) && productPrice <= rangeValue) {
+                    if (!currentRow) {
+                        currentRow = document.createElement("div");
+                        currentRow.className = "product-row";
+                        dynamicContent.appendChild(currentRow);
+                    }
+                    addProductToList(product, currentRow, 'Filtro');
+                    foundResults = true;
+                }
+            }
+
+            if (!foundResults) {
+                const noResultsMessage = document.createElement("p");
+                noResultsMessage.innerText = "Nenhum resultado encontrado.";
+                dynamicContent.appendChild(noResultsMessage);
+            }
+        }
+
+        function sortBy() {
+            const sortOrder = document.getElementById("sort-order").value;
+            const dynamicContent = document.getElementById("product-list");
+            dynamicContent.innerHTML = '';
+
+            let sortedProducts = [...products];
+
+            if (sortOrder === "recentes") {
+                sortedProducts.sort((a, b) => new Date(b.date) - new Date(a.date));
+            } else if (sortOrder === "preco-asc") {
+                sortedProducts.sort((a, b) => {
+                    const priceA = parseFloat(a.price.replace("R$", "").replace(".", "").replace(",", "."));
+                    const priceB = parseFloat(b.price.replace("R$", "").replace(".", "").replace(",", "."));
+                    return priceA - priceB;
+                });
+            } else if (sortOrder === "preco-desc") {
+                sortedProducts.sort((a, b) => {
+                    const priceA = parseFloat(a.price.replace("R$", "").replace(".", "").replace(",", "."));
+                    const priceB = parseFloat(b.price.replace("R$", "").replace(".", "").replace(",", "."));
+                    return priceB - priceA;
+                });
+            } else if (sortOrder === "todos") {
+                // Não faz nada, mantém a ordem original
+            }
+
+            let currentRow;
+
+            for (const product of sortedProducts) {
+                if (!currentRow) {
+                    currentRow = document.createElement("div");
+                    currentRow.className = "product-row";
+                    dynamicContent.appendChild(currentRow);
+                }
+                addProductToList(product, currentRow, 'Ordenado');
+            }
+        }
+
+        window.addEventListener('load', function () {
+            loadDynamicContent('Todos');
+
+            // Corrige o carregamento inicial dos produtos
+            const activeButton = document.querySelector('.active');
+            if (activeButton) {
+                const category = activeButton.innerText;
+                const categoryClass = category.toLowerCase();
+                loadDynamicContent(category, categoryClass);
+            }
+        });
